13b422c1069ecc449702dd8437cbbbc6008e00a1
Ashley Williams <ashley666ashley@gmail.com>
2016-06-29 19:07:48 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const changes = require('concurrent-couch-follower');
+const Request = require('request');
+
+const db = 'https://replicate.npmjs.com';
+
+Request.get(db, function(err, req, body) {
+  var end_sequence = JSON.parse(body).update_seq;
+  var results = [];
+  var bad_words = fs.readFileSync('./bad-words.txt', 'utf-8').split('\n').slice(1, -1);
+  changes(function(change, done) {
+    if (change.seq >= end_sequence) {
+      fs.writeFileSync("results.txt", JSON.stringify(results, null, 2))
+      done();
+    }
+    if (change.doc.name) {
+      console.log("checking:" + change.doc.name);
+      for(var i = 0; i < bad_words.length; i++) {
+        if ((change.doc.name).match(bad_words[i])) {
+          console.log("found " + bad_words[i]);
+          var hit = {
+            "name":  change.doc.name,
+            "word": bad_words[i]
+          };
+          results.push(hit);
+        }
+      }
+    }
+  }, {
+    db: db,
+    include_docs: true
+  })
+});
